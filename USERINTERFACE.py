d0c44c580bac3f7b2303e552ae60fbebe4e8f393
Ai-Mei <purcia.princessmae296@gmail.com>
2023-07-15 15:22:39 +0800

    Added background for the main window and made the icon and background a method in the user interface file.

--- USERINTERFACE.py
+++ USERINTERFACE.py
@@ -1,4 +1,5 @@
 import tkinter as tk
+from PIL import ImageTk, Image
 from ADDENTRY import AddEntry
 from EXISTINGENTRIES import ExistingEntries
 
@@ -10,8 +11,32 @@ class UserInterface:
         add_entry = AddEntry(self.top) 
         existing_entries = ExistingEntries(self.top)
 
-        button1 = tk.Button(frame, text="Add New Entry", command=lambda: add_entry.add_entry(), width=70, height=15)
-        button1.pack(fill=tk.X, expand=True)
+        add_entry = AddEntry(self.top) 
+        existing_entries = ExistingEntries(self.top)
 
-        button2 = tk.Button(frame, text="See Existing Entries", command=lambda: existing_entries.existing_entries(), width=70, height=15)
-        button2.pack(fill=tk.X, expand=True)
+        font_format = ("Century Gothic", 16, "bold")
+
+        button1 = tk.Button(frame, text="Add New Entry", command=lambda: add_entry.add_entry(), width=25, height=2, font=font_format)
+        button1.place(relx=0.5, rely=0.65, anchor=tk.CENTER) 
+
+        button2 = tk.Button(frame, text="See Existing Entries", command=lambda: existing_entries.existing_entries(), width=25, height=2, font=font_format)
+        button2.place(relx=0.5, rely=0.85, anchor=tk.CENTER) 
+
+
+    def background(self, image_path):
+        # Load and resize the background image
+        background_image = Image.open(image_path)
+        background_image = background_image.resize((500, 500), Image.ANTIALIAS)
+
+        # Create a PhotoImage object from the resized image
+        self.background_photo = ImageTk.PhotoImage(background_image)
+
+        # Create a label and set the image as its background
+        background_label = tk.Label(self.top, image=self.background_photo)
+        background_label.place(x=0, y=0, relwidth=1, relheight=1)
+        background_label.lower()
+
+
+    def icon(self, icon_path):
+        self.icon = tk.PhotoImage(file=icon_path)
+        self.top.iconphoto(True, self.icon)
--- main.py
+++ main.py
@@ -6,22 +6,11 @@ top = tk.Tk()
 top.title("TrackrBee")
 top.geometry("500x500")
 
-icon_path = r"C:\Users\Administrator\Documents\2ND SEMESTER\OOP\Dump\FINAL PROJECT\icon.gif"
-icon = tk.PhotoImage(file=icon_path)
-top.iconphoto(True, icon)
-
-canvas = tk.Canvas(top)
-canvas.pack(side=tk.LEFT, fill=tk.BOTH, expand=True)
-scroll_bar = ttk.Scrollbar(top, orient=tk.VERTICAL, command=canvas.yview)
-scroll_bar.pack(side=tk.RIGHT, fill=tk.Y)
-
-canvas.configure(yscrollcommand=scroll_bar.set)
-canvas.bind('<Configure>', lambda e: canvas.configure(scrollregion=canvas.bbox("all")))
-frame = tk.Frame(canvas)
-canvas.create_window((0, 0), window=frame, anchor=tk.NW)
-
 ui = UserInterface(top)
-ui.buttons(frame)
+ui.background(r"C:\Users\Administrator\Documents\2ND SEMESTER\OOP\FINAL PROJECT\TraceBee\bg.png")
+ui.icon(r"C:\Users\Administrator\Documents\2ND SEMESTER\OOP\FINAL PROJECT\TraceBee\icon.gif")
+
+ui.buttons(top)
 
 
 top.mainloop()
